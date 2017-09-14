0c4209f2d1b27c5cc2fa2f8e8c0edf20bc177563
gerswin <gerswin@parawebs.com>
2017-09-14 11:20:46 -0500

    add

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,26 @@
-console.log('\'Allo \'Allo!');
+
+$( '#sendForm' ).on( 'click', function() {
+	$(this).prop('disabled', true);
+	var arr = { first_name: $('#name').val(),
+        last_name: $('#name').val(),
+        email:$('#email').val(),
+        mobile_number:$('#phone').val(),
+        company:$('#company').val(),
+        source:'Web'  
+	}
+	$.ajax({
+		url: 'https://wt-7b4543ad807edea67f96206d71f57f26-0.run.webtask.io/leadsExpoTachira',
+		type: 'POST',
+		data: JSON.stringify(arr),
+		contentType: 'application/json; charset=utf-8',
+		dataType: 'json',
+		async: false,
+		success: function(msg) {
+			$('#name').val('')
+			$('#email').val('')
+			$('#phone').val('')
+			$('#company').val('')
+		    swal('Perfecto!', 'En breve nos pondremos en contacto!', 'success');
+	}
+	});
+});
